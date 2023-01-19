41198b5d45b947787a44a171ddd3ce420e93e85b
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-01-19 17:39:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import First from "./components/first";
+import Second from "./components/second";
+
+function App() {
+  return (
+    <div>
+        <First />
+        <Second />
+    </div>
+  );
+}
+
+export default App;
